003c1799344b1ac7d6436fc89307d8ad444fdead
AlbertSM-dev <lokdok1988@hotmail.com>
2023-10-20 13:30:03 +0100

    Test

--- database.py
+++ database.py
@@ -1,36 +1,38 @@
 import sqlite3
-import pandas as pd
+#import pandas as pd
 
-def db_connect():
-    conn = sqlite3.connect('test_database.sqlite') 
-    c = conn.cursor()
+class Database:
+    def db_connect():
+        conn = sqlite3.connect('test_database.sqlite') 
+        c = conn.cursor()
 
-    c.execute('''
-            CREATE TABLE IF NOT EXISTS COINS
-            ([name] TEXT PRIMARY KEY, [price] TEXT)
-            ''')
+        c.execute('''
+                CREATE TABLE IF NOT EXISTS COINS
+                ([name] TEXT PRIMARY KEY, [price] TEXT)
+                ''')
+                            
+        conn.commit()
+
+    def db_insert(rate):
+        conn = sqlite3.connect('test_database.sqlite') 
+        c = conn.cursor()
+        name="Bitcoin" 
+
+        c.execute("INSERT OR REPLACE INTO COINS (name,price) VALUES(?,?)", (name,rate))
+
+        conn.commit()
+        conn.close()
+
+    def db_show():
+        conn = sqlite3.connect('test_database.sqlite') 
+        c = conn.cursor()
                         
-    conn.commit()
+        c.execute('''
+                SELECT
+                a.name, 
+                a.price
+                FROM COINS a
+                ''')
 
-def db_insert(rate):
-    conn = sqlite3.connect('test_database.sqlite') 
-    c = conn.cursor()
-    name="Bitcoin" 
-
-    c.execute("INSERT OR REPLACE INTO COINS (name,price) VALUES(?,?)", (name,rate))
-
-    conn.commit()
-    conn.close()
-
-def db_show():
-    conn = sqlite3.connect('test_database.sqlite') 
-    c = conn.cursor()
-                    
-    c.execute('''
-            SELECT
-            a.name, 
-            a.price
-            FROM COINS a
-            ''')
-
-    df = pd.DataFrame(c.fetchall(), columns=['name','price'])
+        #df = pd.DataFrame(c.fetchall(), columns=['name','price'])
+        return c.fetchall()
